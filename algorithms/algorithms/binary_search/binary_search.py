88af3d138bbfe9f0d93f7030c4df0623ab7e4c50
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-02-07 20:02:59 +0530

    add binary search

---
+++ algorithms/algorithms/binary_search/binary_search.py
@@ -0,0 +1,24 @@
+# Binary Search Algorithm
+# Time: O(NLogN)
+# Space: O(1)
+
+
+def binary_search(sorted_list, start, end, target):
+    if start <= end:
+        mid = (start + end) // 2
+        if sorted_list[mid] == target:
+            return mid
+        elif sorted_list[mid] < target:
+            return binary_search(sorted_list, mid + 1, end, target)
+        else:
+            return binary_search(sorted_list, start, mid - 1, target)
+
+
+def main():
+    sorted_list = [4, 5, 6, 9, 13, 15]
+    target = 15
+    print(binary_search(sorted_list, 0, len(sorted_list) - 1, target))
+
+
+if __name__ == "__main__":
+    main()
