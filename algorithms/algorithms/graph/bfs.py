f029f870467cfeb707c642d816dad48a113c48c1
Vivek Gandharkar <vivekgandharkar@outlook.com>
2022-09-14 20:26:42 +0530

    update `Graph BFS`

--- algorithms/algorithms/graph/bfs.py
+++ algorithms/algorithms/graph/bfs.py
@@ -1,26 +1,46 @@
+# Time: O(V + E)
+# Space: O(V)
 from algorithms.data_structures.graph.graph import Graph
 
 
-def graph_bfs(s, graph):
-    level = {s: 0}
-    parent = {s: None}
+# Can be used for finding the shortest path between two vertices.
 
-    i = 1
+# Time complexity is O(V + 2E) for undirected graphs and O(V + E) for directed graphs.
 
-    frontier = [s]
 
-    while frontier:
-        next = []
+# def graph_bfs(s, graph):
+#     level = {s: 0}
+#     parent = {s: None}
+#
+#     i = 1
+#
+#     frontier = [s]
+#
+#     while frontier:
+#         next = []
+#
+#         for u in frontier:
+#             for v in graph.associated_vertices(u):
+#                 if v not in level:
+#                     level[v] = i
+#                     parent[v] = u
+#                     next.append(v)
+#
+#         frontier = next
+#         i += 1
 
-        for u in frontier:
-            for v in graph.associated_vertices(u):
-                if v not in level:
-                    level[v] = i
-                    parent[v] = u
-                    next.append(v)
 
-        frontier = next
-        i += 1
+def bfs(s, graph):
+    vertices = [s]
+    visited = set()
+
+    while vertices:
+        vertex = vertices.pop(0)
+
+        if vertex not in visited:
+            print(vertex)
+            visited.add(vertex)
+            vertices.extend(graph.associated_vertices(vertex))
 
 
 def main():
@@ -35,7 +55,8 @@ def main():
     g.add_edge("b", "c")
     g.add_edge("c", "b")
 
-    graph_bfs("a", g)
+    # graph_bfs("a", g)
+    bfs("a", g)
 
 
 if __name__ == "__main__":
